aa9839cf90b97e8db1bea731ce8cdb100282e47c
MahfuzKhandaker <mahfuzkhandaker.bd@email.com>
2023-11-03 13:08:49 +0600

    first commit

---
+++ src/Post.js
@@ -0,0 +1,19 @@
+export default function Post() {
+    return (
+        <div className='post'>
+            <div className="image">
+                <img src="https://www.gstatic.com/webp/gallery3/1.sm.png"></img>
+            </div>
+
+            <div className="texts">
+                <h2>This a blog post title</h2>
+
+                <p className="info">
+                  <a className="author">Mahfuz Khandaker</a>
+                  <time>2023-11-03 1:02</time>
+                </p>
+                  <p className="summary">Today is a time to get learning how we do build a blog using mern stack Today is a time to get learning how we do build a blog using mern stackToday is a time to get learning how we do build a blog using mern stackToday is a time to get learning how we do build a blog using mern stackToday is a time to get learning how we do build a blog using mern stack</p>
+            </div>
+        </div>
+    );
+}
