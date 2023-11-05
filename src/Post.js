d8b9f4a3e799ff4914062cb11cde166935079221
MahfuzKhandaker <mahfuzkhandaker.bd@email.com>
2023-11-05 11:43:11 +0600

    update commit

--- src/Post.js
+++ src/Post.js
@@ -1,19 +1,25 @@
-export default function Post() {
-    return (
-        <div className='post'>
-            <div className="image">
-                <img src="https://www.gstatic.com/webp/gallery3/1.sm.png"></img>
-            </div>
+import {formatISO9075} from "date-fns";
+import {Link} from "react-router-dom";
 
-            <div className="texts">
-                <h2>This a blog post title</h2>
+export default function Post({_id,title,summary,cover,content,createdAt,author}) {
 
-                <p className="info">
-                  <a className="author">Mahfuz Khandaker</a>
-                  <time>2023-11-03 1:02</time>
-                </p>
-                  <p className="summary">Today is a time to get learning how we do build a blog using mern stack Today is a time to get learning how we do build a blog using mern stackToday is a time to get learning how we do build a blog using mern stackToday is a time to get learning how we do build a blog using mern stackToday is a time to get learning how we do build a blog using mern stack</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="post">
+      <div className="image">
+        <Link to={`/post/${_id}`}>
+          <img src={'http://localhost:4000/'+cover} alt=""/>
+        </Link>
+      </div>
+      <div className="texts">
+        <Link to={`/post/${_id}`}>
+        <h2>{title}</h2>
+        </Link>
+        <p className="info">
+          <a className="author">{author.username}</a>
+          <time>{formatISO9075(new Date(createdAt))}</time>
+        </p>
+        <p className="summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
